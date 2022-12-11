f0eb90c7d12786112c4c8a4acf8030a19f477f3b
Ben Kiddie <Ben.Kiddie@globalrelay.net>
2022-12-10 16:07:18 -0800

    Updated rotary diverter valve state keys

--- application/mcu/actuator/rotary_diverter_valve_actuator.py
+++ application/mcu/actuator/rotary_diverter_valve_actuator.py
@@ -9,14 +9,14 @@ class RotaryDiverterValveActuator(BaseActuator, metaclass=ABCMeta):
     @property
     def possible_states(self) -> Dict[str, bytes]:
         return {
-            '1': b'\x00\x00\x00\x01',
-            '2': b'\x00\x00\x00\x02',
-            '3': b'\x00\x00\x00\x03',
-            '4': b'\x00\x00\x00\x04',
-            '5': b'\x00\x00\x00\x05',
-            '6': b'\x00\x00\x00\x06',
+            '0': b'\x00\x00\x00\x01',
+            '1': b'\x00\x00\x00\x02',
+            '2': b'\x00\x00\x00\x03',
+            '3': b'\x00\x00\x00\x04',
+            '4': b'\x00\x00\x00\x05',
+            '5': b'\x00\x00\x00\x06',
         }
 
     @property
     def default_state(self) -> str:
-        return '1'
+        return '0'
