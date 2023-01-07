65adcae9a3d58daf8f24d5080108b5e8a9e53440
diaclectic <bakiddie@gmail.com>
2023-01-07 15:30:56 -0800

    Updated config, minor bugfix in task queue

--- application/model/action/read_sensors_air_loop_action_set.py
+++ application/model/action/read_sensors_air_loop_action_set.py
@@ -12,8 +12,9 @@ class ReadSensorsAirLoopActionSet(ActionSet):
 
         super().__init__(iterable=[
             Action('c7', None),  # Flow rate sensor
-            # Action('c9', None),  # O3 sensor
-            # Action('c0', None),
+            Action('c9', None),  # O3 sensor near generator
+            Action('ca', None),  # O3 sensor in air loop
+            Action('c0', None),
             Action('c1', None),
             Action('c2', None),
             # Action('c8', None),
