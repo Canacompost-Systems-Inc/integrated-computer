fa8e4f9ff13d1fbac410b0c826fe5df639157088
diaclectic <bakiddie@gmail.com>
2023-01-11 12:56:26 -0800

    Removed sensors that are not working

--- application/model/action/read_sensors_air_loop_action_set.py
+++ application/model/action/read_sensors_air_loop_action_set.py
@@ -11,11 +11,11 @@ class ReadSensorsAirLoopActionSet(ActionSet):
         # TODO - uncomment sensors when they are connected
 
         super().__init__(iterable=[
-            Action('c7', None),  # Flow rate sensor
+            # Action('c7', None),  # Flow rate sensor
             Action('c9', None),  # O3 sensor near generator
             Action('ca', None),  # O3 sensor in air loop
             Action('c0', None),
-            Action('c1', None),
+            # Action('c1', None),
             Action('c2', None),
             # Action('c8', None),
         ])
