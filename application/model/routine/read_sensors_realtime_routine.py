d8c81371aa6b0e215a7a82339e32578c2241e792
diaclectic <bakiddie@gmail.com>
2023-01-14 16:38:09 -0800

    Read container sensors realtime

--- application/model/routine/read_sensors_realtime_routine.py
+++ application/model/routine/read_sensors_realtime_routine.py
@@ -1,4 +1,5 @@
 from application.model.action.read_sensors_air_loop_realtime_action_set import ReadSensorsAirLoopRealTimeActionSet
+from application.model.action.read_sensors_container_action_set import ReadSensorsContainerActionSet
 from application.model.routine.routine import Routine
 from application.model.routine.routine_step import RoutineStep
 
@@ -10,8 +11,12 @@ class ReadSensorsRealtimeRoutine(Routine):
 
         super().__init__(
             steps=[
-                # Read the realtime sensors
+                # Read the realtime sensors from the airloop and bioreactors
                 RoutineStep(ReadSensorsAirLoopRealTimeActionSet(), then_wait_n_sec=0),
+                RoutineStep(ReadSensorsContainerActionSet('shredder_storage'), then_wait_n_sec=0),
+                RoutineStep(ReadSensorsContainerActionSet('bioreactor1'), then_wait_n_sec=0),
+                RoutineStep(ReadSensorsContainerActionSet('bioreactor2'), then_wait_n_sec=0),
+                RoutineStep(ReadSensorsContainerActionSet('bsf_reproduction'), then_wait_n_sec=0),
             ],
             must_run_in_state=None,
             failure_recovery_steps=[
