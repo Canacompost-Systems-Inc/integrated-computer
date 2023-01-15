b594ea1969f0c6cd6960776849885899a1ada1e6
diaclectic <bakiddie@gmail.com>
2023-01-14 16:03:18 -0800

    Comment out sanitize routines

--- application/model/routine/sanitize_air_loop_routine.py
+++ application/model/routine/sanitize_air_loop_routine.py
@@ -20,23 +20,23 @@ class SanitizeAirLoopRoutine(Routine):
 
         super().__init__(
             steps=[
-                # Remove any bypasses
-                RoutineStep(SwitchAirLoopBypassSensorLoopActionSet('divert'), then_wait_n_sec=0),
-                # Turn on the ozone generator and let it build up
-                RoutineStep(SwitchOzoneGeneratorActionSet('on'), then_wait_n_sec=30),
-                # Turn on the UVC light
-                RoutineStep(SwitchUVCLightActionSet('on'), then_wait_n_sec=0),
-                # Begin circulating the ozone
-                RoutineStep(SwitchAirMoverActionSet('on'), then_wait_n_sec=10),
-                # Stop producing ozone
-                RoutineStep(SwitchOzoneGeneratorActionSet('off'), then_wait_n_sec=10),
-                # Flush the ozone from the system
-                RoutineStep(SwitchAirLoopEnvironmentExchangeActionSet(strength='100'), then_wait_n_sec=30),
-                # End sequence
-                RoutineStep(SwitchUVCLightActionSet('off'), then_wait_n_sec=0),
-                RoutineStep(SwitchAirLoopEnvironmentExchangeActionSet(strength='0'), then_wait_n_sec=0),
-                RoutineStep(SwitchAirMoverActionSet('off'), then_wait_n_sec=10),
-                RoutineStep(SwitchAirLoopBypassSensorLoopActionSet('through'), then_wait_n_sec=0),
+                # # Remove any bypasses
+                # RoutineStep(SwitchAirLoopBypassSensorLoopActionSet('divert'), then_wait_n_sec=0),
+                # # Turn on the ozone generator and let it build up
+                # RoutineStep(SwitchOzoneGeneratorActionSet('on'), then_wait_n_sec=30),
+                # # Turn on the UVC light
+                # RoutineStep(SwitchUVCLightActionSet('on'), then_wait_n_sec=0),
+                # # Begin circulating the ozone
+                # RoutineStep(SwitchAirMoverActionSet('on'), then_wait_n_sec=10),
+                # # Stop producing ozone
+                # RoutineStep(SwitchOzoneGeneratorActionSet('off'), then_wait_n_sec=10),
+                # # Flush the ozone from the system
+                # RoutineStep(SwitchAirLoopEnvironmentExchangeActionSet(strength='100'), then_wait_n_sec=30),
+                # # End sequence
+                # RoutineStep(SwitchUVCLightActionSet('off'), then_wait_n_sec=0),
+                # RoutineStep(SwitchAirLoopEnvironmentExchangeActionSet(strength='0'), then_wait_n_sec=0),
+                # RoutineStep(SwitchAirMoverActionSet('off'), then_wait_n_sec=10),
+                # RoutineStep(SwitchAirLoopBypassSensorLoopActionSet('through'), then_wait_n_sec=0),
             ],
             must_run_in_state=DefaultState,
             failure_recovery_steps=[
--- application/model/routine/sanitize_compost_loop_routine.py
+++ application/model/routine/sanitize_compost_loop_routine.py
@@ -4,6 +4,7 @@ from application.model.action.switch_air_loop_environment_exchange_action_set im
     SwitchAirLoopEnvironmentExchangeActionSet
 from application.model.action.switch_air_mover_action_set import SwitchAirMoverActionSet
 from application.model.action.switch_ozone_generator_action_set import SwitchOzoneGeneratorActionSet
+from application.model.action.switch_uvc_light_action_set import SwitchUVCLightActionSet
 from application.model.routine.routine import Routine
 from application.model.routine.routine_step import RoutineStep
 
@@ -19,6 +20,8 @@ class SanitizeCompostLoopRoutine(Routine):
                 # RoutineStep(SwitchAirLoopBypassSensorLoopActionSet('divert'), then_wait_n_sec=0),
                 # # Turn on the ozone generator and let it build up
                 # RoutineStep(SwitchOzoneGeneratorActionSet('on'), then_wait_n_sec=30),
+                # # Turn on the UVC light
+                # RoutineStep(SwitchUVCLightActionSet('on'), then_wait_n_sec=0),
                 # # Begin circulating the ozone
                 # RoutineStep(SwitchAirMoverActionSet('on'), then_wait_n_sec=10),
                 # # Stop producing ozone
@@ -26,11 +29,13 @@ class SanitizeCompostLoopRoutine(Routine):
                 # # Flush the ozone from the system
                 # RoutineStep(SwitchAirLoopEnvironmentExchangeActionSet(strength='100'), then_wait_n_sec=30),
                 # # End sequence
+                # RoutineStep(SwitchUVCLightActionSet('off'), then_wait_n_sec=0),
                 # RoutineStep(SwitchAirLoopEnvironmentExchangeActionSet(strength='0'), then_wait_n_sec=0),
                 # RoutineStep(SwitchAirMoverActionSet('off'), then_wait_n_sec=10),
             ],
             must_run_in_state=None,
             failure_recovery_steps=[
+                RoutineStep(SwitchUVCLightActionSet('off'), then_wait_n_sec=0),
                 RoutineStep(SwitchOzoneGeneratorActionSet('off'), then_wait_n_sec=10),
                 RoutineStep(SwitchAirLoopEnvironmentExchangeActionSet(strength='100'), then_wait_n_sec=30),
                 RoutineStep(SwitchAirLoopEnvironmentExchangeActionSet(strength='0'), then_wait_n_sec=0),
